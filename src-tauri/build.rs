0213a0f17ebb01c597b920eb389158f1f94bf0ec
Demir Yerli <mrquantumoff@protonmail.com>
2023-01-07 20:54:22 +0300

    dependency updates

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,24 +1,4 @@
 // extern crate winres;
-#[cfg(target_os = "windows")]
-use winres::WindowsResource;
 fn main() {
-    #[cfg(target_os = "windows")]
-    {
-        let mut res = WindowsResource::new();
-        res.set_manifest(
-            r#"
-<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
-<trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
-    <security>
-        <requestedPrivileges>
-            <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
-        </requestedPrivileges>
-    </security>
-</trustInfo>
-</assembly>
-"#,
-        );
-        res.compile().unwrap();
-    }
     tauri_build::build()
 }
